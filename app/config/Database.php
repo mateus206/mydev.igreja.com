650ccb3dd0d1a5aa2e04dababebd9ab93224ca03
GerryMateusMTrindade <madeiramateus520@gmail.com>
2026-05-06 15:27:40 +0100

    sla

---
+++ app/config/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+class DataBase {
+  private $host = "localhost";
+  private $db_name = "mydevigreja.com";
+  private $username = "root";
+  private $password = "";
+
+  public function connect() {
+    $conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $conn;
+  }
+}  
